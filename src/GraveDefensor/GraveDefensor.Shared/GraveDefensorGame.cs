b743204b905d1a9f98e080fd4870afcfd4db9c9b
Miha Markič <miha@rthand.com>
2018-09-24 13:59:08 +0200

    Code cleaning

--- src/GraveDefensor/GraveDefensor.Shared/GraveDefensorGame.cs
+++ src/GraveDefensor/GraveDefensor.Shared/GraveDefensorGame.cs
@@ -7,7 +7,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
-using System.Diagnostics;
 
 namespace GraveDefensor.Shared
 {
