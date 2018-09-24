6e1af43c527aa23ca0ae10ec216f09be20f6638d
Miha Markič <miha@rthand.com>
2018-09-24 16:13:21 +0200

    Closes #11 Adds Pro and All solutions, also Windows.Pro and Android.Pro projects that use non oss content

---
+++ src/GraveDefensor/GraveDefensor.Windows.Pro/Program.cs
@@ -0,0 +1,27 @@
+﻿using GraveDefensor.Engine.Core;
+using GraveDefensor.Shared;
+using System;
+
+namespace GraveDefensor.Windows
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ScreenInfo.Default = ScreenInfo.Window(768, 1280, hasMouse: true);
+            using (var game = new GraveDefensorGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
