a428990a667d5ae081391d02a3b7b74ff5f6a887
Miha Markič <miha@rthand.com>
2019-11-02 16:23:40 +0100

    Migrates from Windows to Desktop/.NET Core 3 - closes #13

--- src/GraveDefensor/GraveDefensor.Desktop/Program.cs
+++ src/GraveDefensor/GraveDefensor.Desktop/Program.cs
@@ -2,26 +2,16 @@
 using GraveDefensor.Shared;
 using System;
 
-namespace GraveDefensor.Windows
+namespace GraveDefensor.Desktop
 {
-#if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             ScreenInfo.Default = ScreenInfo.Window(768, 1280, hasMouse: true);
             using (var game = new GraveDefensorGame())
-            {
                 game.Run();
-            }
         }
     }
-#endif
 }
