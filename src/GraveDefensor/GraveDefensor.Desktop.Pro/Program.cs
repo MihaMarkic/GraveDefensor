b2f2d45b0f022f2c37340e20d46772b821ad73d4
Miha Markič <miha@rthand.com>
2019-11-02 16:30:09 +0100

    Migrates from Windows.Pro to Desktop.Pro/.NET Core 3 - closes #14

---
+++ src/GraveDefensor/GraveDefensor.Desktop.Pro/Program.cs
@@ -0,0 +1,17 @@
+﻿using GraveDefensor.Engine.Core;
+using GraveDefensor.Shared;
+using System;
+
+namespace GraveDefensor.Desktop.Pro
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ScreenInfo.Default = ScreenInfo.Window(768, 1280, hasMouse: true);
+            using (var game = new GraveDefensorGame())
+                game.Run();
+        }
+    }
+}
